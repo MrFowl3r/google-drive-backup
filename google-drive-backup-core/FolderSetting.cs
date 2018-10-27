1ac6b210f9b7c42abf95882a4f4e5268181542b5
MrFowl3r <jas.fowler+gitrepo@gmail.com>
2018-10-27 19:02:44 +1300

    Initial commit

---
+++ google-drive-backup-core/FolderSetting.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace google_drive_backup_core
+{
+    /// <summary>
+    /// A class for what path to backup and file patterns in this path to ignore.
+    /// </summary>
+    public class FolderSetting
+    {
+        public string Path { get; set; }
+        public List<string> IgnorePatterns { get; set; }
+    }
+}
---
+++ google-drive-backup-core/FolderSettingLoader.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace google_drive_backup_core
+{
+    /// <summary>
+    /// A class to load/save folder settings to a json formatted file.
+    /// </summary>
+    public class FolderSettingLoader
+    {
+        public FolderSettingLoader(string fullSettingsFileName)
+        {
+            _settingsFileName = fullSettingsFileName;
+        }
+
+        private string _settingsFileName;
+
+        /// <summary>
+        /// Load folder settings from json formatted file.
+        /// </summary>
+        /// <returns>A list of folder settings. If file doesn't exist null is returned.</returns>
+        public List<FolderSetting> Load()
+        {
+            if (File.Exists(_settingsFileName))
+            {
+                string jsonContent = File.ReadAllText(_settingsFileName);
+                return JsonConvert.DeserializeObject<List<FolderSetting>>(jsonContent);
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Save folder settings in json formatted file.
+        /// </summary>
+        /// <param name="settings">A list of folder settings</param>
+        public void Save(List<FolderSetting> settings)
+        {
+            string jsonContent = JsonConvert.SerializeObject(settings);            
+            File.WriteAllText(_settingsFileName, jsonContent);  // Create/overwrite the file
+        }
+    }
+}
